cf88a8b6cd8ecc2890117db288f0439679011ade
karanba <altaykarakus@gmail.com>
2015-03-15 14:30:14 +0000

    added less files

---
+++ app/modules/ngKid/grid/grid.js
@@ -0,0 +1,20 @@
+angular.module('ngkidgrid', []).directive('ngKidGrid', function () {
+    return {
+        restrict: 'EA',
+        replace: false,
+        templateUrl: 'app/modules/ngKid/grid/grid.html',
+        controller: ['$scope', function ($scope) {
+            $scope.width = "50px";
+            $scope.columns = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+            $scope.rows = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+        }],
+        link: function(scope, element){
+            var head = $('.pi-gr-hd', element);
+            var body = $('.pi-gr-bd', element);
+            body.unbind('scroll');
+            body.bind('scroll', function(){
+                head.css('left', -body.scrollLeft());
+            });
+        }
+    };
+});
