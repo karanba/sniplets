b4ce6c50e64acd38ebfa046a089a37ebc55ff575
karanba <altaykarakus@gmail.com>
2015-03-15 14:59:04 +0000

    working on resizble columns

--- app/modules/ngKid/grid/grid.js
+++ app/modules/ngKid/grid/grid.js
@@ -1,20 +1,28 @@
-angular.module('ngkidgrid', []).directive('ngKidGrid', function () {
-    return {
-        restrict: 'EA',
-        replace: false,
-        templateUrl: 'app/modules/ngKid/grid/grid.html',
-        controller: ['$scope', function ($scope) {
-            $scope.width = "50px";
-            $scope.columns = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
-            $scope.rows = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
-        }],
-        link: function(scope, element){
-            var head = $('.pi-gr-hd', element);
-            var body = $('.pi-gr-bd', element);
-            body.unbind('scroll');
-            body.bind('scroll', function(){
-                head.css('left', -body.scrollLeft());
-            });
-        }
-    };
-});
+angular.module('ngkidgrid', [])
+    .directive('ngKidGrid', function() {
+        return {
+            restrict: 'EA',
+            replace: false,
+            templateUrl: 'app/modules/ngKid/grid/grid.html',
+            controller: ['$scope', function($scope) {
+                $scope.width = "50px";
+                $scope.columns = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+                $scope.rows = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+            }],
+            link: {
+                post: function(scope, element) {
+                    var head = $('.pi-gr-hd', element);
+                    var body = $('.pi-gr-bd', element);
+
+                    // sync scroll of body with head 
+                    body.unbind('scroll');
+                    body.bind('scroll', function() {
+                        head.css('left', -body.scrollLeft());
+                    });
+
+                    //resizable columns
+                    $('.pi-gr-col').resizable();
+                }
+            }
+        };
+    }); // todo: col directiove
